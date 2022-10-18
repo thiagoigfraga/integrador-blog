89ab1640a61757be62a6e53671587f0b90353ce4
Thiago Garcia <thiagogarciafraga99@gmail.com>
2022-10-18 00:37:06 -0300

    Configurações firebase

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className={styles.footer}>
       <h3>Tudo do que o mundo pode oferecer.</h3>
-      <p>Blog &copy; 2022</p>
+      <p>MRT Blog &copy; 2022</p>
     </footer>
   );
 }
